fd55e778a06a7b8a29dabc1d97517f5596807d12
Megan Go <meganggo11@gmail.com>
2021-07-26 18:57:03 +0000

    labels added and timestamp

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -1,4 +1,5 @@
 let googleUser = null //makes a global variable so you can use the user's info throughout the file
+let createdTags = ""
 
 window.onload = () => {
     firebase.auth().onAuthStateChanged(user =>{
@@ -14,17 +15,36 @@ window.onload = () => {
         }
     })
 
+    const workButton = document.querySelector("#workButton");
+    const personalButton = document.querySelector("#personalButton");
+    const tutorialsButton = document.querySelector("#tutorialsButton");
+
+    workButton.addEventListener("click", ()=>{
+            createdTags+="work ";
+    })
+
+    personalButton.addEventListener("click", ()=>{
+        createdTags+="personal ";
+    })
+
+    tutorialsButton.addEventListener("click", ()=>{
+        createdTags+="tutorials ";
+    })
+
     const createNoteButton = document.querySelector("#createNote");
     createNoteButton.addEventListener("click", () =>{
        //get values from the notes form
         const noteTitle = document.querySelector("#noteTitle").value;
         const noteText = document.querySelector("#noteText").value;
-        console.log(noteTitle, noteText);
+        const currentTime= new Date() + Math.floor(Date.now()/1000);
+        console.log(noteTitle, noteText, currentTime);
 
         //write values to database
         firebase.database().ref(`/users/${googleUser.uid}`).push({
             title: noteTitle,
-            text: noteText
+            text: noteText,
+            created: currentTime,
+            tags: createdTags
         }).then(()=>{ //runs after the note is added to the database, resets values to null
             console.log("database write successful")
             document.querySelector("#noteTitle").value = null
